bbdced7ff7b1209a5de902cfee8d18012dd5b401
shamuiscoding <toebee@snu.ac.kr>
2024-08-28 20:25:28 -0700

    chore: upgrade version to 0.3.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="promptfile",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_namespace_packages(),
     entry_points={},
     description="promptfile: language support for .prompt files",
